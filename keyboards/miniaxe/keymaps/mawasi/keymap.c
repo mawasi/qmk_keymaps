941a4ad443438c63e707cc4a66e4bea95b7860ac
mawasi <mawasi99@hotmail.com>
2022-11-06 22:54:36 +0900

    qmk_firmware tag 0.18.16 くらいのバージョンでビルドエラーになるのを修正。

--- keyboards/miniaxe/keymaps/mawasi/keymap.c
+++ keyboards/miniaxe/keymaps/mawasi/keymap.c
@@ -16,10 +16,12 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+enum layer_names {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -42,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |Ctrl/Esc| LOWER|Sft/Spc|        |Sft/Spc| RAISE| Alt |
  *               `--------------------'           `--------------------'
  */
-[_QWERTY] = LAYOUT( \
+[_QWERTY] = LAYOUT_split_3x5_3(
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |      | LOWER|      |           |      | RAISE|      |
  *               `--------------------'           `--------------------'
  */
-[_RAISE] = LAYOUT( \
+[_RAISE] = LAYOUT_split_3x5_3(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ESC,      KC_LGUI, KC_PGUP, KC_UP, KC_PGDN, KC_BSPC, \
   KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL,      KC_HOME, KC_LEFT, KC_DOWN,  KC_RGHT, KC_ENT, \
   KC_LCTL, KC_LSFT, KC_LALT, XXXXXXX, XXXXXXX,      KC_END, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, \
@@ -80,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |      | LOWER|      |           |      | RAISE|      |
  *               `--------------------'           `--------------------'
  */
-[_LOWER] = LAYOUT( \
+[_LOWER] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
   KC_TAB,  KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,      KC_SCLN, KC_QUOT, KC_LBRC, KC_RBRC, KC_ENT, \
   KC_LCTL, KC_LSFT, KC_LALT, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
@@ -99,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |      | LOWER|      |           |      | RAISE| Reset|
  *               `--------------------'           `--------------------'
  */
-[_ADJUST] =  LAYOUT( \
+[_ADJUST] =  LAYOUT_split_3x5_3(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
   KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX,      LALT(KC_GRV), KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
@@ -148,11 +150,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-void led_set_user(uint8_t usb_led) {
-}
